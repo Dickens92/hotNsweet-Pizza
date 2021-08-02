aea99cb3fbcc59468409b0225031d82fb1d499a5
philo <philopodickens92@gmail.com>
2021-08-02 11:23:45 +0300

    add switch and constructors to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -16,11 +16,11 @@ $(document).ready(function(){
    let pname = $(".name option:selected").val();
    let psize = $("#size option:selected").val();
    let pcrust = $("#crust option:selected").val();
-   let ptopping = [];
+   let pizzatopping = [];
    $.each($("input[name='toppings']:checked"), function(){            
-       ptopping.push($(this).val());
+       pizzatopping.push($(this).val());
    });
-   console.log(ptopping.join(", "));
+   console.log(pizzatopping.join(", "));
 
    switch(psize){
     case "0":
@@ -56,7 +56,7 @@ $(document).ready(function(){
       default:
         console.log("No price"); 
     }
-    let topping_value = ptopping.length*50;
+    let topping_value = pizzatopping.length*100;
     console.log("toppins value" + topping_value);
 
     if((psize == "0") && (pcrust == "0")){
@@ -64,7 +64,7 @@ $(document).ready(function(){
       $("button.proceed").show();
       $("#information").show();
       $("div.choise").hide();
-      alert("Please select pizza size and crust"); 
+      alert("Kindly chose your preffered size and crust or contact us for Guidance"); 
     }
     else{
       $("button.proceed").hide();
@@ -80,33 +80,33 @@ $(document).ready(function(){
     $("#pizzaname").html($(".name option:selected").val());
     $("#pizzasize").html( $("#size option:selected").val());
     $("#pizzacrust").html($("#crust option:selected").val());
-    $("#pizzatopping").html(ptopping.join(", "));
+    $("#pizzatoppings").html(pizzatoppings.join(", "));
     $("#totals").html(total);
     
-// Add pizza button
+
     $("button.addPizza").click(function(){
       let pname = $(".name option:selected").val();
       let psize = $("#size option:selected").val();
       let pcrust = $("#crust option:selected").val();
-      let ptopping = [];
+      let pizzatopping = [];
       $.each($("input[name='toppings']:checked"), function(){            
-          ptopping.push($(this).val());
+          pizzatopping.push($(this).val());
       });
-      console.log(ptopping.join(", "));
+      console.log(pizzatopping.join(", "));
       switch(psize){
         case "0":
           price =0;
         break;
         case "large":
-           price = 1200;
+           price = 1800;
            console.log(price);
          break;
          case "medium":
-           price = 850;
+           price = 1200;
            console.log("The price is "+price);
          break;
          case "small":
-           price = 600;
+           price = 720;
            console.log(price);
          default:
            console.log("error"); 
@@ -115,13 +115,13 @@ $(document).ready(function(){
           case "0":
             crust_price = 0;
           break;
-          case "Crispy":
+          case "Flat Bread":
             crust_price = 200;
           break;
-          case "Stuffed":
+          case "Cheese Crust":
             crust_price = 150;
           break;
-          case "Gluten-free":
+          case "Staffed Crust":
             crust_price = 180;
           break;
           default:
