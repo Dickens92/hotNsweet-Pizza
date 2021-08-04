f49623b2755a6de3a9d74c4fe37eb367b622fc19
philo <philopodickens92@gmail.com>
2021-08-04 03:35:38 +0300

    updated files to replace the previous ones which were unresponsive on the site

--- js/scripts.js
+++ js/scripts.js
@@ -9,18 +9,18 @@ function Deliverpizza( name,size,crust,topping, total ){
 }
 
 
-// proceed button
+
 $(document).ready(function(){
   
   $("button.proceed").click(function(event){
    let pname = $(".name option:selected").val();
    let psize = $("#size option:selected").val();
    let pcrust = $("#crust option:selected").val();
-   let topping = [];
+   let ptopping = [];
    $.each($("input[name='toppings']:checked"), function(){            
-       topping.push($(this).val());
+       ptopping.push($(this).val());
    });
-   console.log(topping.join(", "));
+   console.log(ptopping.join(", "));
 
    switch(psize){
     case "0":
@@ -44,32 +44,32 @@ $(document).ready(function(){
       case "0":
         crust_price = 0;
       break;
-      case "Flat Bread":
-        crust_price = 200;
+      case "Flatbread":
+        crust_price = 90;
       break;
-      case "Cheese Crust":
-        crust_price = 250;
+      case "Cheesecrust":
+        crust_price = 120;
       break;
-      case "Staffed Crust":
-        crust_price = 180;
+      case "Staffedcrust":
+        crust_price = 150;
       break;
       default:
         console.log("No price"); 
     }
-    let topping_value = topping.length*100;
-    console.log("topping value" + topping_value);
+    let topping_value = ptopping.length*100;
+    console.log("toppins value" + topping_value);
 
     if((psize == "0") && (pcrust == "0")){
       console.log("nothing selected");
       $("button.proceed").show();
       $("#information").show();
-      $("div.select").hide();
-      alert("Kindly chose your preffered size and crust or contact us for Guidance"); 
+      $("div.choise").hide();
+      alert("Dear Customer! Select Your Preffered Size and Crust. Contact us for Further Guidance"); 
     }
     else{
       $("button.proceed").hide();
       $("#information").hide();
-      $("div.select").slideDown(1000);
+      $("div.choise").slideDown(1000);
     }
 
     total = price + crust_price + topping_value;
@@ -80,7 +80,7 @@ $(document).ready(function(){
     $("#pizzaname").html($(".name option:selected").val());
     $("#pizzasize").html( $("#size option:selected").val());
     $("#pizzacrust").html($("#crust option:selected").val());
-    $("#pizzatoppings").html(ptoppings.join(", "));
+    $("#pizzatopping").html(ptopping.join(", "));
     $("#totals").html(total);
     
 
@@ -92,7 +92,7 @@ $(document).ready(function(){
       $.each($("input[name='toppings']:checked"), function(){            
           ptopping.push($(this).val());
       });
-      console.log(toppings.join(", "));
+      console.log(ptopping.join(", "));
       switch(psize){
         case "0":
           price =0;
@@ -115,27 +115,27 @@ $(document).ready(function(){
           case "0":
             crust_price = 0;
           break;
-          case "Flat Bread":
+          case "Flatbread":
             crust_price = 90;
           break;
-          case "Cheese Crust":
+          case "Cheesecrust":
             crust_price = 120;
           break;
-          case "Staffed Crust":
+          case "Staffedcrust":
             crust_price = 150;
           break;
           default:
             console.log("No price"); 
         }
         let topping_value = ptopping.length*100;
-        console.log("toppings value" + topping_value);
+        console.log("toppins value" + topping_value);
         total = price + crust_price + topping_value;
         console.log(total);
 
         checkoutTotal = checkoutTotal + total;
         console.log(checkoutTotal);
-      // constractor function
-      var newOrder = new Deliverpizza(pname, psize, pcrust,pizzatoppings,total);
+      
+      var newOrder = new Deliverpizza(pname, psize, pcrust,ptopping,total);
 
       $("#customerorders").append('<tr><td id="pizzaname">'+newOrder.name +'</td><td id="pizzasize">' + newOrder.size + '</td><td id="pizzacrust">'+newOrder.crust + '</td><td id="pizzatopping">'+newOrder.topping+'</td><td id="totals">'+newOrder.total+'</td></tr>');
       console.log(newOrder);
@@ -143,51 +143,51 @@ $(document).ready(function(){
       
 
     });
-    // Checkout button
+    
     $("button#checkout").click(function(){ 
       $("button#checkout").hide();
       $("button.addPizza").hide();
       $("button.send").slideDown(1000);
       $("#addedprice").slideDown(1000);
-      console.log("Please pay a total of Ksh. "+checkoutTotal);
+      console.log("Your total bills is sh. "+checkoutTotal);
       $("#pizzatotal").append("Your bill is sh. "+checkoutTotal);
     });
 
-    // home delivery button
-    $("button.Send").click(function(){
-      $(".packedpizza").hide();
-      $(".select h2").hide();
-      $(".Send").slideDown(1000);
+    
+    $("button.send").click(function(){
+      $(".pizzatable").hide();
+      $(".choise h2").hide();
+      $(".sending").slideDown(1000);
       $("#addedprice").hide();
-      $("button.Send").hide();
+      $("button.send").hide();
       $("#pizzatotal").hide();
-      let sendingcost = checkoutTotal+150;
-      console.log("The fullamount comes to Ksh. "+sendingcost+" on delivery");
-      $("#totalbill").append("The bill plus delivery fee is: "+ sendingcost);
+      let sendingcost= checkoutTotal+150;
+      console.log("You will pay sh. "+deliceryamount+" on delivery");
+      $("#totalbill").append("Full amount: "+sendingcost);
     });
 
-    // when one clicks place order button
-    $("button#final-order").click(function(event){
+    
+    $("button#last-order").click(function(event){
       event.preventDefault();
 
       $("#pizzatotal").hide();
-      $(".delivery").hide();
+      $(".sending").hide();
       $("button#last-order").hide();
       let sendingcost= checkoutTotal+150;
       console.log("Final Bill is: "+sendingcost);
       let person = $("input#name").val();
       let phone = $("input#phone").val();
-      let location = $("input#Pickup-location").val();
+      let place = $("input#place").val();
 
-      if ($("input#name").val() && $("input#phone").val() && $("input#Pickup-location").val()!=""){
+      if ($("input#name").val() && $("input#phone").val() && $("input#place").val()!=""){
   
-        $("#notification").append(person+", Your Order Details have been Received and after processing, it will be sent to "+location+ ". Carry Ksh. "+sendingcost);
+        $("#notification").append(person+", Your order is complete. After processing, your pizza will be Delivered to "+place+ ". Prepare sh. "+sendingcost);
         $("#totalbill").hide();
         $("#notification").slideDown(1200);
       }
       else {
-        alert("Kindly provide details to aid in delivery of your order!");
-        $(".send").show();
+        alert("Enter your personal details as listed below to aid in delivery!");
+        $(".delivery").show();
         $("button#last-order").show();
       }
     });
